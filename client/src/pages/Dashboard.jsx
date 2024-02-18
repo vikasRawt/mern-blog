7ad930bec15dbc23dfdcee7a90633e71fec80ddd
vikas <vicky582050@gmail.com>
2024-02-18 18:49:36 +0530

    first commit

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      Dash
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div>
+      Projecs
+      
+    </div>
+  );
+}
+
+export default Projects;
