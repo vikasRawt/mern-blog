35b3bbd2c0a9446e02391de6f7a8d4de29358398
vikas <vicky582050@gmail.com>
2024-02-29 12:28:12 +0530

    jwt comp commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 
 export const signUp = async (req, res, next) => {
   const { username, password, email } = req.body;
@@ -32,3 +34,38 @@ export const signUp = async (req, res, next) => {
     next(error);
   }
 };
+
+
+
+export const signIn = async (req,res,next)=>{
+const{email, password} = req.body;
+
+if(!email|| !password || email ===" " || password ===" "){
+ return next(errorHandler(400,"All feilds required"));
+}
+try {
+  const validuser = await User.findOne({email});
+
+  if(!validuser){
+   return next(errorHandler(404,"User not found"));
+  }
+
+  const validPassword = bcryptjs.compareSync(password,validuser.password);
+  
+  if(!validPassword){
+   return next(errorHandler(400,"Invalid password or email"));
+  }
+
+  const token = jwt.sign({
+    id:validuser._id
+  }, process.env.JWT_SECRET);
+  
+
+  const {password : pass, ...rest} = validuser._doc;
+  res.status(200).cookie('access_token', token,{
+    httpOnly: true
+  }).json(rest);
+} catch (error) {
+  next(error)
+}
+}
