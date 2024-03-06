65824a41362d91bbf92cefdbed5a54dc67ebb3d9
vikas <vicky582050@gmail.com>
2024-03-06 23:18:21 +0530

    dark mode  commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,14 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa6";
-
-import { useSelector } from "react-redux"; //tells if a user logged in//
-
+import { FaMoon, FaSun } from "react-icons/fa6";
+import { useSelector, useDispatch } from "react-redux"; //tells if a user logged in//
+import { toggleTheme } from "../reduxApp/theme/themeSlice";
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
-
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -32,36 +32,48 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="grey" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10 hidden sm:inline"
+          color="grey"
+          pil
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme=== "light" ? <FaSun /> : <FaMoon />}
         </Button>
-          {currentUser ? (
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={
-                <Avatar alt="user" img={currentUser.data.profilePicture} rounded />
-              }
-            >
-              <Dropdown.Header>
-                <span className="block text-sm">@{currentUser.data.username}</span>
-                <span className="block text-sm font-medium truncate">
-                  @{currentUser.data.email}
-                </span>
-              </Dropdown.Header>
-              <Link to="/dashboard?tab=profile">{/* understand this */}
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="user"
+                img={currentUser.data.profilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">
+                @{currentUser.data.username}
+              </span>
+              <span className="block text-sm font-medium truncate">
+                @{currentUser.data.email}
+              </span>
+            </Dropdown.Header>
+            <Link to="/dashboard?tab=profile">
+              {/* understand this */}
               <Dropdown.Item>Profile</Dropdown.Item>
-              </Link>
-              <Dropdown.Divider/>
-              <Dropdown.Item>Sign Out</Dropdown.Item>
-            </Dropdown>
-          ) : (
-            <Link to="/sign-in">
-              <Button gradientDuoTone="purpleToBlue" outline>
-                Sign In
-              </Button>
             </Link>
-          )}
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
--- client/src/reduxApp/store.js
+++ client/src/reduxApp/store.js
@@ -1,12 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import useReducer from "../reduxApp/User/userSlice";
-import {persistReducer} from "redux-persist";
-
+import { persistReducer } from "redux-persist";
+import themeReducer from "./theme/themeSlice";
 import storage from "redux-persist/lib/storage";
 import { persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({
   user: useReducer,
+  theme: themeReducer,
 });
 
 const persistConfig = {
